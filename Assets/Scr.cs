ab5137cc436f0aa83c076f4d342b18f9f6925527
Tim Hemmingsen <Tim Hemmingsen>
2017-04-02 21:54:18 +0200

    .

---
+++ Assets/Scr.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr : MonoBehaviour {
+    
+    public Transform target;
+    private UnityEngine.AI.NavMeshAgent agent;
+    
+
+	void Start () {
+       agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        agent.SetDestination(target.position);
+	}
+}
